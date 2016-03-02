68d3d85c587e971a0a24759439a763b55644bcf9
Revan Sopher <rsopher@gmail.com>
2016-03-01 21:00:03 -0500

    Docstrings for networking library.

--- clustermessaging/Messager.py
+++ clustermessaging/Messager.py
@@ -87,26 +87,49 @@ class Messager:
 
 
     def getNeighbors(self):
+        """
+        Iterate over names with getNeighbors().keys()
+        :return: the dict of names to sockets
+        """
         return self.neighbors
 
     def getAddr(self, name):
+        """
+        :raises RuntimeError if network topology forbids this link
+        :param name: the name to query
+        :return: the address of the specified node
+        """
         if name in self.addresses:
             addr = '%s:%d' % (self.addresses[name], self._findPortFor(name))
-            print('getAddr(%s): %s' %(name, addr))
             return addr
         else:
             raise RuntimeError('No link between me and %s in topology!' % name)
 
     def getSocket(self, name):
+        """
+        :raises RuntimeError if network topology forbids this link
+        :param name: the name to query
+        :return: the socket between self and other node
+        """
         if name in self.neighbors:
             return self.neighbors[name]
         else:
             raise RuntimeError('No link between me and %s in topology!' % name)
 
     def sendMessage(self, name, message):
+        """
+        Sends a message to a node.
+        :param name: node to send to
+        :param message: arbitrary python object to be sent
+        """
         self.getSocket(name).send_pyobj(message)
 
     def registerCallbackIndividual(self, callbackFunction, name):
+        """
+        Register an async callback on a specific neighbor. Use registerCallback() to register on all neighbors.
+        :param callbackFunction: function taking two parameters, message and name.
+        :param name: neighbor we're registering on
+        """
         def decorator(data):
             message = pickle.loads(b''.join(data))
             callbackFunction(message, name)
@@ -117,10 +140,19 @@ class Messager:
         self.stream.on_recv(decorator, copy=True)
 
     def registerCallback(self, callbackFunction):
+        """
+        Register an async callback on every neighbor.
+
+        :param callbackFunction: function taking two parameters, message (arbitrary python object) and
+                                 name (name of node who sent this message)
+        """
         for name in self.neighbors:
             if name is not self.getOwnName():
                 self.registerCallbackIndividual(callbackFunction, name)
 
     def start(self):
+        """
+        Starts the event loop in a background thread. Call this once, after having set the callback.
+        """
         threading.Thread(target=self.loop.start).start()
 
