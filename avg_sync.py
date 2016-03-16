4162f551abdf2a205f85b90f8a50c0f4c4aae91b
Nikhil-Shenoy <nikhil_shenoy10@yahoo.com>
2016-03-16 16:55:15 -0400

    finally got synchronous average after stupid error

--- avg_sync.py
+++ avg_sync.py
@@ -4,17 +4,30 @@ from threading import Lock
 import random
 from clustermessaging.Messager import Messager
 import os
+import csv
 
-assignment = {
-	"1": 6,
-	"2": 9,
-	"3": 12
-}
+# assignment = {
+# 	"1": 6,
+# 	"2": 9,
+# 	"3": 12
+# }
+
+# Used to deal with infinity values
+MAXINT = 65535 
+
+# Initialize lock and Messager objects
+m = Messager()
+m.registerCallbackSync()
+m.start()
+
+# Get list of neighbors
+neighbors = list(m.getNeighbors().keys())
 
 ID = int(os.environ["DEVICE_ID"])
 # Get value assignment
-my_val = assignment[str(ID)]
-# print("My val is {0}".format(my_val))
+# my_val = assignment[str(ID)]
+my_val = float(np.random.randint(100))
+print("Starting val is: {0}".format(my_val))
 
 def get_incidence_matrix(topo):
 	n = len(topo)
@@ -62,98 +75,53 @@ def generate_stochastic_matrix(topo):
 
 	return stoc_mat
 
-def all_vals_received(vals):
-	total = 0
-	for num in vals:
-		if vals[num] is not None:
-			total += 1
+def get_weights(m,ID):
+	# Get stochastic matrix 
+	W = generate_stochastic_matrix(m.topo)
+	# print("W is {0}".format(W))
 
-	if total == len(neighbors):
-		return True
-	else:
-		return False
+	# Get own weights
+	w = W[ID-1]
 
-def reset_received_vals(vals):
-	for key in vals:
-		vals[key] = None
-
-def callback(message,name):
-	# print("Got {0} from {1}. My ID is {2}".format(message["value"],message["sender"],ID))
-	global node_val_map
-
-	if node_val_map[name] is None:
-		node_val_map[name] = message['value']
-
-	if message['rsvp']:
-		print('Replying...')
-		reply = {
-			'value': my_val,
-			'rsvp': False
-		}
-		m.sendMessage(name,reply)
-
-# Used to deal with infinity values
-MAXINT = 65535 
-
-# Initialize lock and Messager objects
-lock = Lock()
-m = Messager()
-m.registerCallback(callback)
-m.start()
+	return w
 
 
-# Get list of neighbors
-neighbors = list(m.getNeighbors().keys())
-# print("My neighbors are {0}".format(neighbors))
-
-node_val_map = {}
-for n in neighbors:
-	node_val_map[n] = None
-
-# Get stochastic matrix 
-W = generate_stochastic_matrix(m.topo)
-# print("The stochastic matrix is {0}".format(W))
-# Get stochastic matrix 
-W = generate_stochastic_matrix(m.topo)
-# print("The stochastic matrix is {0}".format(W))
-
-# Get own weights
-w = W[ID-1]
-# print("My weights are {0}".format(w))
+w = get_weights(m,ID)
+# print("w is {0}".format(w))
 
 # Generate x vector
 nodes = len(neighbors) + 1
 x = np.zeros([nodes,1],dtype=float)
-# print("My x is {0}".format(x))
 
 # Insert own value
 x[ID-1] = my_val
-# print("The new x is {0}".format(x))
 
-
-iterations = 3
-
-for i in range(iterations):
-	# Send my_val to all neighbors
-	time.sleep(1)
-	# All of the communication
-	for recipient in neighbors:
+iterations = 100
+for iter in range(iterations):
+	# All of the communication to neighbors
+	for recipient in m.getNeighbors().keys():
 		message = {
 			'value' : my_val,
-			'rsvp'  : True
+			'sync'  : iter
 		}
-		print("Sending {0} to {1} from {2}".format(my_val,recipient,ID))
+		# print("Sending {0} to {1} from {2}".format(my_val,recipient,ID))
 		m.sendMessage(recipient,message)
 
-	print("The node_value_map is {0}".format(node_val_map))
-	
-	# Construct X
-	for key in node_val_map:
-		x[int(key)-1] = node_val_map[key]
+	# print("Waiting for all values to arrive")
+	m.waitForMessageFromAllNeighbors(iter)
 
-	print("x is {0}".format(x))
+	# print("Got all values. Constructing x vector")
+	# Construct X
+	for message in m.sync[iter]:
+		node = int(message['from'])
+		value = float(message['value'])
+
+		x[node-1] = value
+
+
+	# print("w is {0}, x is {1}".format(w,x))
 	my_val = np.dot(w,x)[0]
-	reset_received_vals(node_val_map)
+	print("my_val is {0}".format(my_val))
 
 
 
