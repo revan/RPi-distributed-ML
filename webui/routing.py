75520d67729813bcc0839fe155b4ae017273a112
Revan Sopher <rsopher@gmail.com>
2016-04-26 03:00:50 -0400

    Live-updating error plot.

--- webui/routing.py
+++ webui/routing.py
@@ -1,9 +1,13 @@
 import json
-from flask import Flask, render_template, request
+import gevent
+from gevent import monkey
+monkey.patch_all()
+from flask import Flask, render_template, request, Response
 app = Flask(__name__)
 
 version = 0
 topology = {i: [] for i in range(1, 16)}
+classifier_errors = []
 
 @app.route('/')
 def main():
@@ -25,5 +29,24 @@ def topo():
 
     return json.dumps(topology)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+def error_stream():
+    global classifier_errors
+    while True:
+        yield 'data: %s\n\n' % classifier_errors
+        gevent.sleep(0.2)
+
+@app.route('/classifier_stream/', methods=['GET', 'POST', 'DELETE'])
+def class_stream():
+    global classifier_errors
+    if request.method == 'POST':
+        classifier_errors.append(float(request.form['value']))
+        return "OK"
+    elif request.method == 'DELETE':
+        classifier_errors = []
+        return "OK"
+    else:
+        return Response(error_stream(), mimetype="text/event-stream")
+
+@app.route('/svm')
+def svm():
+    return render_template('svm.html')
